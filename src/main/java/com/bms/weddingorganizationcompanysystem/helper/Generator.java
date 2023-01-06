45ba600c8fdb742b6f679118d01d2f73425b163e
Berkay <berkaymehmetsert@gmail.com>
2023-01-06 16:17:57 +0300

    Add generate code helper

---
+++ src/main/java/com/bms/weddingorganizationcompanysystem/helper/Generator.java
@@ -0,0 +1,23 @@
+package com.bms.weddingorganizationcompanysystem.helper;
+
+import com.bms.weddingorganizationcompanysystem.helper.message.BusinessMessage;
+
+import java.util.Random;
+
+public class Generator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final Random RANDOM = new Random();
+    private static final StringBuilder CODE = new StringBuilder();
+    private static final Integer LENGTH = 16;
+
+    private Generator() {
+        throw new IllegalStateException(BusinessMessage.ILLEGAL_STATE_EXCEPTION);
+    }
+
+    public static String generateCode(){
+        for (int i = 0; i < LENGTH; i++) {
+            CODE.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return CODE.toString();
+    }
+}
