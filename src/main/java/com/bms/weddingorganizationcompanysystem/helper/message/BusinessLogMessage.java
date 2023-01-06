6e761c32480f97b35f3801b0bfb891065d138abb
Berkay <berkaymehmetsert@gmail.com>
2023-01-06 15:58:47 +0300

    Add new field

--- src/main/java/com/bms/weddingorganizationcompanysystem/helper/message/BusinessLogMessage.java
+++ src/main/java/com/bms/weddingorganizationcompanysystem/helper/message/BusinessLogMessage.java
@@ -17,7 +17,8 @@ public class BusinessLogMessage {
         public static final String CITY_CREATED = "City created successfully. City name: ";
         public static final String CITY_UPDATED = "City updated successfully. City id: ";
         public static final String CITY_DELETED = "City deleted successfully. City id: ";
-        public static final String CITY_LISTEd = "City listed successfully.";
+        public static final String CITY_LISTED = "City listed successfully.";
+        public static final String CITY_FOUND = "City found successfully. City id: ";
     }
 
     public static class Country {
@@ -32,6 +33,7 @@ public class BusinessLogMessage {
         public static final String COUNTRY_UPDATED = "Country updated successfully. Country id: ";
         public static final String COUNTRY_DELETED = "Country deleted successfully. Country id: ";
         public static final String COUNTRY_LISTED = "Country listed successfully.";
+        public static final String COUNTRY_FOUND = "Country found successfully. Country id: ";
     }
 
     public static class Employment {
@@ -46,6 +48,7 @@ public class BusinessLogMessage {
         public static final String EMPLOYMENT_UPDATED = "Employment updated successfully. Employment id: ";
         public static final String EMPLOYMENT_DELETED = "Employment deleted successfully. Employment id: ";
         public static final String EMPLOYMENT_LISTED = "Employment listed successfully.";
+        public static final String EMPLOYMENT_FOUND = "Employment found successfully. Employment id: ";
     }
 
     public static class EmploymentInclude {
@@ -59,6 +62,7 @@ public class BusinessLogMessage {
         public static final String EMPLOYMENT_INCLUDE__UPDATED = "EmploymentInclude updated successfully. EmploymentInclude id: ";
         public static final String EMPLOYMENT_INCLUDE__DELETED = "EmploymentInclude deleted successfully. EmploymentInclude id: ";
         public static final String EMPLOYMENT_INCLUDE__LISTED = "EmploymentInclude listed successfully.";
+        public static final String EMPLOYMENT_INCLUDE__FOUND = "EmploymentInclude found successfully. EmploymentInclude id: ";
     }
 
     public static class EmploymentProvider {
@@ -72,6 +76,7 @@ public class BusinessLogMessage {
         public static final String EMPLOYMENT_PROVIDER_UPDATED = "EmploymentProvider updated successfully. EmploymentProvider id: ";
         public static final String EMPLOYMENT_PROVIDER_DELETED = "EmploymentProvider deleted successfully. EmploymentProvider id: ";
         public static final String EMPLOYMENT_PROVIDER_LISTED = "EmploymentProvider listed successfully.";
+        public static final String EMPLOYMENT_PROVIDER_FOUND = "EmploymentProvider found successfully. EmploymentProvider id: ";
     }
 
     public static class Event {
@@ -85,6 +90,7 @@ public class BusinessLogMessage {
         public static final String EVENT_UPDATED = "Event updated successfully. Event id: ";
         public static final String EVENT_DELETED = "Event deleted successfully. Event id: ";
         public static final String EVENT_LISTED = "Event listed successfully.";
+        public static final String EVENT_FOUND = "Event found successfully. Event id: ";
     }
 
     public static class InEvent {
@@ -98,7 +104,7 @@ public class BusinessLogMessage {
         public static final String IN_EVENT_UPDATED = "InEvent updated successfully. InEvent id: ";
         public static final String IN_EVENT_DELETED = "InEvent deleted successfully. InEvent id: ";
         public static final String IN_EVENT_LISTED = "InEvent listed successfully.";
-
+        public static final String IN_EVENT_FOUND = "InEvent found successfully. InEvent id: ";
     }
 
     public static class Invoice {
@@ -112,6 +118,7 @@ public class BusinessLogMessage {
         public static final String INVOICE_UPDATED = "Invoice updated successfully. Invoice id: ";
         public static final String INVOICE_DELETED = "Invoice deleted successfully. Invoice id: ";
         public static final String INVOICE_LISTED = "Invoice listed successfully.";
+        public static final String INVOICE_FOUND = "Invoice found successfully. Invoice id: ";
     }
 
     public static class InvoiceItem {
@@ -125,6 +132,7 @@ public class BusinessLogMessage {
         public static final String INVOICE_ITEM_UPDATED = "InvoiceItem updated successfully. InvoiceItem id: ";
         public static final String INVOICE_ITEM_DELETED = "InvoiceItem deleted successfully. InvoiceItem id: ";
         public static final String INVOICE_ITEM_LISTED = "InvoiceItem listed successfully.";
+        public static final String INVOICE_ITEM_FOUND = "InvoiceItem found successfully. InvoiceItem id: ";
     }
 
     public static class Location {
@@ -138,6 +146,7 @@ public class BusinessLogMessage {
         public static final String LOCATION_UPDATED = "Location updated successfully. Location id: ";
         public static final String LOCATION_DELETED = "Location deleted successfully. Location id: ";
         public static final String LOCATION_LISTED = "Location listed successfully.";
+        public static final String LOCATION_FOUND = "Location found successfully. Location id: ";
     }
 
     public static class Participate {
@@ -151,6 +160,7 @@ public class BusinessLogMessage {
         public static final String PARTICIPATE_UPDATED = "Participate updated successfully. Participate id: ";
         public static final String PARTICIPATE_DELETED = "Participate deleted successfully. Participate id: ";
         public static final String PARTICIPATE_LISTED = "Participate listed successfully.";
+        public static final String PARTICIPATE_FOUND = "Participate found successfully. Participate id: ";
     }
 
     public static class Partner {
@@ -164,6 +174,7 @@ public class BusinessLogMessage {
         public static final String PARTNER_UPDATED = "Partner updated successfully. Partner id: ";
         public static final String PARTNER_DELETED = "Partner deleted successfully. Partner id: ";
         public static final String PARTNER_LISTED = "Partner listed successfully.";
+        public static final String PARTNER_FOUND = "Partner found successfully. Partner id: ";
     }
 
     public static class Person {
@@ -178,6 +189,7 @@ public class BusinessLogMessage {
         public static final String PERSON_UPDATED = "Person updated successfully. Person id: ";
         public static final String PERSON_DELETED = "Person deleted successfully. Person id: ";
         public static final String PERSON_LISTED = "Person listed successfully.";
+        public static final String PERSON_FOUND = "Person found successfully. Person id: ";
     }
 
     public static class Product {
@@ -192,6 +204,7 @@ public class BusinessLogMessage {
         public static final String PRODUCT_UPDATED = "Product updated successfully. Product id: ";
         public static final String PRODUCT_DELETED = "Product deleted successfully. Product id: ";
         public static final String PRODUCT_LISTED = "Product listed successfully.";
+        public static final String PRODUCT_FOUND = "Product found successfully. Product id: ";
     }
 
     public static class ProductInclude {
@@ -205,6 +218,7 @@ public class BusinessLogMessage {
         public static final String PRODUCT_INCLUDE_UPDATED = "ProductInclude updated successfully. ProductInclude id: ";
         public static final String PRODUCT_INCLUDE_DELETED = "ProductInclude deleted successfully. ProductInclude id: ";
         public static final String PRODUCT_INCLUDE_LISTED = "ProductInclude listed successfully.";
+        public static final String PRODUCT_INCLUDE_FOUND = "ProductInclude found successfully. ProductInclude id: ";
     }
 
     public static class ProductProvider {
@@ -218,6 +232,7 @@ public class BusinessLogMessage {
         public static final String PRODUCT_PROVIDER_UPDATED = "ProductProvider updated successfully. ProductProvider id: ";
         public static final String PRODUCT_PROVIDER_DELETED = "ProductProvider deleted successfully. ProductProvider id: ";
         public static final String PRODUCT_PROVIDER_LISTED = "ProductProvider listed successfully.";
+        public static final String PRODUCT_PROVIDER_FOUND = "ProductProvider found successfully. ProductProvider id: ";
     }
 
     public static class Role {
@@ -232,6 +247,7 @@ public class BusinessLogMessage {
         public static final String ROLE_UPDATED = "Role updated successfully. Role id: ";
         public static final String ROLE_DELETED = "Role deleted successfully. Role id: ";
         public static final String ROLE_LISTED = "Role listed successfully.";
+        public static final String ROLE_FOUND = "Role found successfully. Role id: ";
     }
 
     public static class Status {
@@ -246,6 +262,7 @@ public class BusinessLogMessage {
         public static final String STATUS_UPDATED = "Status updated successfully. Status id: ";
         public static final String STATUS_DELETED = "Status deleted successfully. Status id: ";
         public static final String STATUS_LISTED = "Status listed successfully.";
+        public static final String STATUS_FOUND = "Status found successfully. Status id: ";
     }
 
     public static class Wedding {
@@ -259,5 +276,6 @@ public class BusinessLogMessage {
         public static final String WEDDING_UPDATED = "Wedding updated successfully. Wedding id: ";
         public static final String WEDDING_DELETED = "Wedding deleted successfully. Wedding id: ";
         public static final String WEDDING_LISTED = "Wedding listed successfully.";
+        public static final String WEDDING_FOUND = "Wedding found successfully. Wedding id: ";
     }
 }
